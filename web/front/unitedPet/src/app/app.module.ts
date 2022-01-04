835f39748bbd6dcec9fc7930126b2679b6ab981c
jouinibilel <bileljouinipro@hotmail.com>
2022-01-04 19:46:36 +0100

    testing components

--- web/front/unitedPet/src/app/app.module.ts
+++ web/front/unitedPet/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent
   ],
   imports: [
     BrowserModule,
