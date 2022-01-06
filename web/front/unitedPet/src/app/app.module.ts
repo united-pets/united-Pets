58dbd0faa0abcaa782aed5c01e38e44b3809eeb0
Ali-boubaker <alibenboubaker111@gmail.com>
2022-01-06 14:17:01 +0100

    first commit

--- web/front/unitedPet/src/app/app.module.ts
+++ web/front/unitedPet/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
 ;
 
 @NgModule({
   declarations: [
     AppComponent,
+    ItemsComponent,
 
   ],
   imports: [
