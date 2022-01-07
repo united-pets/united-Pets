22f1fa2906107fefafe4cc5d3488f640d14eebfc
aladin-jridi <aladinjridi007@gmail.com>
2022-01-07 19:14:07 +0100

    uplode img

--- web/front/unitedPet/src/app/app.module.ts
+++ web/front/unitedPet/src/app/app.module.ts
@@ -5,15 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { AngularFireModule } from '@angular/fire/compat/';
-// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-// import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-// import { AngularFireModule } from '@angular/fire';
-// import { AngularFireDatabaseModule } from '@angular/fire/database';
-// import { AngularFireStorageModule } from '@angular/fire/storage';
-// import { environment } from '../environments/environment';
 
+import { AngularFireModule } from '@angular/fire/compat/';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProfileComponent } from './profile/profile.component';
 import { UpdateProfileComponent } from './update-profile/update-profile.component';
@@ -37,13 +33,19 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     HttpClientModule,
     NgbModule,
-    // AngularFireModule.initializeApp(environment.firebase),
-    // AngularFireDatabaseModule,
-    // AngularFireStorageModule
-    // AngularFirestoreModule ,
-    //    AngularFireModule.initializeApp(environment.firebase),
-    //     AngularFireDatabaseModule,
-    //     AngularFireStorageModule,
+    AngularFirestoreModule ,
+       AngularFireModule.initializeApp({
+        apiKey: "AIzaSyBMiLuyb1jsTJ7EqmGt0TJOy9qDndfFLec",
+        authDomain: "copper-harbor-331713.firebaseapp.com",
+        projectId: "copper-harbor-331713",
+        storageBucket: "copper-harbor-331713.appspot.com",
+        messagingSenderId: "1032981236332",
+        appId: "1:1032981236332:web:ef77379dc4fcb214732649",
+        measurementId: "G-JHRVKT56RF"
+      }),
+        AngularFireDatabaseModule,
+        AngularFireStorageModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
