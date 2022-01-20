cb7193a8e7fc215fb9e86d04228978f979bd0f2c
aladin-jridi <aladinjridi007@gmail.com>
2022-01-20 14:56:26 +0100

    fix err of inner join

--- server/controllers/getStore.js
+++ server/controllers/getStore.js
@@ -1,14 +1,15 @@
 var db = require("../database/index.js")
 module.exports={
     GetStore: (req, res) => {
-        console.log(req.params.user_iduser);
+        // console.log(req.params.user_iduser);
         db.query(
             "SELECT * FROM `Stores` where `user_iduser` = ?",[req.params.user_iduser],
             (err, result) => {
                 if (err) {
                     console.log(err);
+                    res.send(err)
                 } else {
-                    console.log(result.length);
+                    // console.log(result.length);
                     res.send(result);
                 }
             }
